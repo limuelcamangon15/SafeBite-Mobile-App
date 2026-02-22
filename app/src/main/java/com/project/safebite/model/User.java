60628391c0d50e68639e20c070a9149e20fc41aa
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-02-23 00:26:16 +0800

    update: updated user model and added search listener to barcode edittxt

--- app/src/main/java/com/project/safebite/model/User.java
+++ app/src/main/java/com/project/safebite/model/User.java
@@ -5,8 +5,7 @@ import java.util.List;
 
 public class User {
 
-    private String firstName;
-    private String lastName;
+    private String fullName;
     private String email;
     private String password;
     private List<String> allergies;
@@ -16,20 +15,12 @@ public class User {
         this.registeredAt = LocalDateTime.now();
     }
 
-    public String getFirstName() {
-        return firstName;
+    public String getFullName() {
+        return fullName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
 
     public String getEmail() {
--- app/src/main/java/com/project/safebite/ui/fragment/ScanFragment.java
+++ app/src/main/java/com/project/safebite/ui/fragment/ScanFragment.java
@@ -8,9 +8,12 @@ import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
@@ -74,7 +77,24 @@ public class ScanFragment extends Fragment {
 
     private void initializeViews(View view){
         btnScan = view.findViewById(R.id.btnScan);
+        etBarcode = view.findViewById(R.id.etBarcode);
 
         btnScan.setOnClickListener(v -> askCameraPermission());
+
+        etBarcode.setOnEditorActionListener((v, actionId, event) -> {
+            if(actionId == EditorInfo.IME_ACTION_SEARCH ||
+                    (event != null && event.getKeyCode() == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN)
+            ){
+                searchProduct(etBarcode.getText().toString());
+
+                return true;
+            }
+            return false;
+        });
+    }
+    private void searchProduct(String barcode) {
+        UIUtil.showSnackbar(parent,barcode);
+
+        //TODO: GET req to openfoodfacts api here
     }
 }
